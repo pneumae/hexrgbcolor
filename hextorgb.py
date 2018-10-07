d24fde77f228866c2d3ca718b61808954b66f001
Hong <hong226@gmail.com>
2018-10-07 12:53:37 +0200

    Improve user input parser

--- hextorgb.py
+++ hextorgb.py
@@ -6,16 +6,28 @@ def HexRgb(value):
     b = int("".join(valueList[4:6]),16)
     print(r,g,b)
 
-def RgbHex(value) #argument is array
-    r = hex(value[0])
-    g = hex(value[1])
-    b = hex(value[2])
+def RgbHex(value):
+    r = hex(int(value[0]))[2:]
+    g = hex(int(value[1]))[2:]
+    b = hex(int(value[2]))[2:]
     print(r + g + b)
 
 
 rawString = input()
 numberList = rawString.split(" ")
-#print(numberlist)
+#print(numberList)
 
-for i in range(0,len(numberList)):
-    HexRgb(numberList[i])
+for i in range(0,len(numberList)-2):
+#    print(i)
+    if(len(numberList[i]) == 6):
+        HexRgb(numberList[i])
+        continue
+    if(len(numberList[i]) <= 3):
+        if(len(numberList[i+1]) <= 3 and len(numberList[i+2]) <= 3):
+            decArray = [numberList[i],numberList[i+1],numberList[i+2]]
+            numberList[i] = decArray[:]
+            a = i+1
+            b = i+3
+            del numberList[i+1:b]
+            RgbHex(numberList[i])
+
