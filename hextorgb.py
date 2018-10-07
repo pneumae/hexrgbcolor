3652b561c6643f9dec44f341053fdbb875bb6f09
Hong <hong226@gmail.com>
2018-10-07 11:56:02 +0200

    Add dec to hex convertor

--- hextorgb.py
+++ hextorgb.py
@@ -1,14 +1,21 @@
-def hextorgb(value):
-    valuelist = list(value)
+def HexRgb(value):
+    valueList = list(value)
     #slice array, join values, turn hex to decimal
-    r = int("".join(valuelist[0:2]),16)
-    g = int("".join(valuelist[2:4]),16)
-    b = int("".join(valuelist[4:6]),16)
+    r = int("".join(valueList[0:2]),16)
+    g = int("".join(valueList[2:4]),16)
+    b = int("".join(valueList[4:6]),16)
     print(r,g,b)
+
+def RgbHex(value) #argument is array
+    r = hex(value[0])
+    g = hex(value[1])
+    b = hex(value[2])
+    print(r + g + b)
+
 
 rawString = input()
 numberList = rawString.split(" ")
 #print(numberlist)
 
 for i in range(0,len(numberList)):
-    hextorgb(numberList[i])
+    HexRgb(numberList[i])
